6e521d5575712769123474c001b98456911de46d
root <root@v-db-smoke05.sz.kingdee.net>
2019-08-21 17:27:07 +0800

    jmx监控方式优化，由客户端本机监控改为oms统一监控

--- sync_component.py
+++ sync_component.py
@@ -37,11 +37,10 @@ result = json.loads(response.read())
 
 if result['code'] == 0:
     service_list = []
-    jmxport_list = []       #jmx端口需要特殊处理
+    jmx_info = {}
     #先清除/etc/corn.d目录下所有的cron文件
-    cmd = 'cd /etc/cron.d && find . -name "*.cron" |grep -v "ntp.cron" |xrags -i rm -rf {}'
+    cmd = 'cd /etc/cron.d && find . -name "*.cron" |grep -v "ntp.cron" |xargs -i rm -rf {}'
     commands.getoutput(cmd)
-
     #清空端口列表文件
     cmd = 'cd %s/portmon && rm -rf list.txt'%cur_dir
     commands.getoutput(cmd)
@@ -53,8 +52,16 @@ if result['code'] == 0:
         fadmin_user = i['fadmin_user']
         fadmin_password = i['fadmin_password']
 
+        #收集ip和端口，用来统一监控端口状态
+        cmd = "echo '%s,%s' >> %s/portmon/list.txt"%(fhost, fport, cur_dir)
+        commands.getoutput(cmd)
+
         if fname == 'jmx':
-            jmxport_list.append(fport)
+            if jmx_info.has_key(fhost):
+                jmx_info[fhost].append(fport)
+            else:
+                jmx_info[fhost] = [fport]
+            continue
 
         if fname not in service_list:
             cmd = "echo '%s,%s,%s,%s' > %s/%smon/list.txt"%(fhost, fport, fadmin_user, fadmin_password, cur_dir, fname)
@@ -71,24 +78,28 @@ if result['code'] == 0:
         
 
 
-        #收集ip和端口，用来统一监控端口状态
-        cmd = "echo '%s,%s' >> %s/portmon/list.txt"%(fhost, fport, cur_dir)
-        commands.getoutput(cmd)    
     cmd = "echo '* * * * * root (cd %s/portmon && python port_monitor.py list.txt)' >/etc/cron.d/port.cron"%(cur_dir)
     print cmd
     commands.getoutput(cmd)
 
 
 
-    '''
-    if jmxport_list:
-        jmxport_list = list(set(jmxport_list))
-        cmd = 'cd %s/jmxmon && cp -rf conf.example.properties conf.properties && sed -i "s/jmx.ports=/jmx.ports=%s/g" conf.properties && ./control restart'%(
-            cur_dir, ','.join(jmxport_list)
-        )
-        print cmd
+    
+    for jmx_host, port_list in jmx_info.items():
+        port_list = list(set(port_list))
+        
+
+        cmd = 'cd {0}/jmxmon && cp -rf conf.example.properties conf.properties.{1} && \
+            sed -i "s/jmx.ports=/jmx.ports={2}/g" conf.properties.{1} && \
+            sed -i "s/hostname=/hostname={1}/g" conf.properties.{1}'.format(cur_dir, jmx_host, ','.join(port_list))
         commands.getoutput(cmd)
-    '''        
+
+        cmd = "echo '* * * * * root (cd {0}/jmxmon && export jmx_host={1} && \
+            timeout 10 java -cp ./jmxmon-0.0.2-jar-with-dependencies.jar com.stephan.tof.jmxmon.JMXMonitor  conf.properties.{1})' \
+            >> /etc/cron.d/jmx.cron".format(cur_dir, jmx_host)
+
+        commands.getoutput(cmd)
+
 
 
 
